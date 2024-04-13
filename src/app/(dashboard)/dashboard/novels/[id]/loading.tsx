99988a13f7d85b7eaf357845cf553afdddcef647
apihdotpy <74187080+apihdotpy@users.noreply.github.com>
2024-04-13 23:19:26 +0800

    Update loading component to display "loading..." message

--- src/app/(dashboard)/dashboard/novels/[id]/loading.tsx
+++ src/app/(dashboard)/dashboard/novels/[id]/loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-  return <div className="grid min-h-screen place-items-center"></div>
+  return <div className="grid min-h-screen place-items-center">loading...</div>
 }
